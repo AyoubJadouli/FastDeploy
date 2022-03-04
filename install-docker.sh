87a4b362daea5711a36ac447dbdbace76a1b1372
AyoubJadouli <46377718+AyoubJadouli@users.noreply.github.com>
2022-03-04 10:22:07 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get remove -y docker docker-engine docker.io containerd runc 
+sudo apt-get update -y
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -9,14 +9,14 @@ sudo apt-get install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsbping_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
- sudo apt-get update
- sudo apt-get install docker-ce docker-ce-cli containerd.io
+ sudo apt-get update -y
+ sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
  
  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
  
- sudo mkdir /docker
+ sudo mkdir /docker 2> /dev/null || ls /docker
  sudo chmod +x /usr/local/bin/docker-compose
  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
  docker-compose --version
