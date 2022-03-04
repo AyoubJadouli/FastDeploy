86098e5e3334965742c6656c85d878869f11ca58
AyoubJadouli <46377718+AyoubJadouli@users.noreply.github.com>
2022-03-04 10:30:08 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -8,8 +8,8 @@ sudo apt-get install -y \
     lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsbping_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
  sudo apt-get update -y
  sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
