d6c99cf69814ac552ba85d18e014faa8addc3836
AyoubJadouli <46377718+AyoubJadouli@users.noreply.github.com>
2022-06-06 16:52:34 +0100

    Create debug-tools.sh

---
+++ debug-tools.sh
@@ -0,0 +1,17 @@
+sudo apt-get update -y
+sudo apt-get install -y \
+    pv \
+    htop \
+    nmap \
+    curl \
+    wget \
+    zip unzip \
+    rar unrar \
+    gnupg \
+    python3 python3-pip\
+    lsb-release
+    
+ # sudo apt install python-is-python3 || echo no package py-is-py3
+  
+  # bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+  # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
